ef39d327743e6d7abfc77082b4087570ed74a333
Lina <lina@Linas-MacBook-Pro.local>
2025-12-04 04:47:30 +0900

    feat: MovieCard 클릭 시 /details로 이동하도록 라우팅 구현

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom';
+
 function MovieCard({ title, poster, rating }) {
     return (
-        <div className="movie-card">
-            <img src={`https://image.tmdb.org/t/p/w500${poster}`} alt={title} />
-            <h3>{title}</h3>
-            <p>평점: {rating}</p>
-        </div>
+        <Link to="/details">
+            <div className="movie-card">
+                <img src={`https://image.tmdb.org/t/p/w500${poster}`} alt={title} />
+                <h3>{title}</h3>
+                <p>평점: {rating}</p>
+            </div>
+        </Link>
     );
 }
 
