b914db72f9fff9c6bd0b56bad414729891ac8d65
Lina <lina@Linas-MacBook-Pro.local>
2025-12-04 15:45:12 +0900

    fix: Route 상대경로로 변경 및 Home.jsx로 파일명 변경

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <React.StrictMode> {/* 엄근진 모드 레츠고 */}
+    <BrowserRouter> {/* 이 안에 들어가면 라우터 기능 사용 가능 */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
